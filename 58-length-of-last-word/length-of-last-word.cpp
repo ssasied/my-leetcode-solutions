bab99d306faa83b8f0e77f59cc28f7cf61a7cc68
ssasied <115064635+ssasied@users.noreply.github.com>
2024-03-28 16:38:13 +0100

    Time: 0 ms (100.00%) | Memory: 7.8 MB (20.40%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -4,9 +4,8 @@ public:
         int ss=0;
         for(int i=s.size()-1;i>=0;i--){
             if(s[i]!=' ')ss++;
-            else{
-                if(ss)return ss;
-            }
+            else if(ss)return ss;
+            
         }
         return ss;
     }
