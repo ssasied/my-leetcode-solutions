fe8e2606b1bf2111a13609d8355ce7e56850bc42
ssasied <115064635+ssasied@users.noreply.github.com>
2024-04-03 14:20:07 +0200

    Time: 0 ms (100.00%) | Memory: 10.5 MB (26.17%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -2,65 +2,20 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         string result="";
-        bool carry=false;
+        int carry=0;
         int ita=a.size()-1;
         int itb=b.size()-1;
-        while(ita>=0&&itb>=0){
-            // cout<<a[ita]<<" "<<b[itb]<<'\n';
-            // if(carry)cout<<1<<'\n';
-            // else cout<<0<<'\n';
-            if(a[ita]=='1'&&b[itb]=='1'){
-                if(carry)result='1'+result;
-                else result='0'+result;
-                carry=true;
+        while(ita>=0||itb>=0||carry){
+            if(ita>=0){
+                carry+=a[ita--]-'0';
             }
-            else if(a[ita]=='1'&&b[itb]=='0'){
-                if(carry)result='0'+result;
-                else result='1'+result;
-            }
-            else if(a[ita]=='0'&&b[itb]=='1'){                
-                if(carry)result='0'+result;
-                else result='1'+result;
-            }
-            else if(a[ita]=='0'&&b[itb]=='0'){                
-                if(carry)result='1'+result;
-                else result='0'+result;
-                carry=false;
-            }
-            ita--;
-            itb--;
+            if(itb>=0){
+                carry+=b[itb--]-'0';
+            }      
+            char a=carry%2+'0';     
+            result=a+result;
+            carry*=0.5;
         }
-        if(itb>=0){
-            for(int i=itb;i>=0;i--){
-            //     cout<<b[itb]<<'\n';
-            // if(carry)cout<<1<<'\n';
-            // else cout<<0<<'\n';
-                if(b[i]=='1'){
-                    if(carry)result='0'+result;
-                    else {result='1'+result;carry=false;}
-                }
-                else if(b[i]=='0'){
-                    carry?result='1'+result:result='0'+result;
-                    carry=false;                
-                }
-            }
-        }
-        else{
-            for(int i=ita;i>=0;i--){
-            //     cout<<a[ita]<<'\n';
-            // if(carry)cout<<1<<'\n';
-            // else cout<<0<<'\n';
-                if(a[i]=='1'){
-                    if(carry)result='0'+result;
-                    else {result='1'+result;carry=false;}
-                }
-                else if(a[i]=='0'){
-                    carry?result='1'+result:result='0'+result;
-                    carry=false;                
-                }
-            }
-        }
-        if(carry)result='1'+result;
         return result;
     }
 };
