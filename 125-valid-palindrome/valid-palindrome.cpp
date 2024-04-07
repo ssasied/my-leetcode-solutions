901b35d31ec16fb6292dd0b9b25d6f1cab5cdfb9
ssasied <115064635+ssasied@users.noreply.github.com>
2024-04-07 16:42:40 +0200

    Time: 0 ms (100.00%) | Memory: 8.7 MB (63.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,16 +4,18 @@ public:
         bool isp=true;
         int l=0;
         int r=s.size()-1;
-        while(l<r){
-            while(!((s[l]>='a'&&s[l]<='z')||(s[l]>='A'&&s[l]<='Z')||(s[l]>='0'&&s[l]<='9'))&&l<r){
-                l++;
-            }
-            while(!((s[r]>='a'&&s[r]<='z')||(s[r]>='A'&&s[r]<='Z')||(s[r]>='0'&&s[r]<='9'))&&r>l){
-                r--;
-            }
+        while(l<r){            
             s[l]=tolower(s[l]);
             s[r]=tolower(s[r]);
-            if(!(s[l]==s[r])){isp=false;break;}
+            while(!((s[l]>='a'&&s[l]<='z')||(s[l]>='0'&&s[l]<='9'))&&l<r){                
+                l++;
+            s[l]=tolower(s[l]);
+            }
+            while(!((s[r]>='a'&&s[r]<='z')||(s[r]>='0'&&s[r]<='9'))&&r>l){
+                r--;                
+                s[r]=tolower(s[r]);
+            }
+            if(!(s[l]==s[r])){isp=false;cout<<s[l]<<l<<' '<<s[r]<<r;break;}
             l++;
             r--;
         }
