c03c1a5cf61bad6ab620756de1e77929d00bdd1a
ssasied <115064635+ssasied@users.noreply.github.com>
2024-04-07 15:28:48 +0200

    Time: 99 ms (11.34%) | Memory: 95.7 MB (69.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,12 +4,8 @@ public:
         int current_min=prices[0];
         int max_dif=0;
         for(int i=1;i<prices.size();i++){
-            if(prices[i]<current_min){
-                current_min=prices[i];
-            }
-            else if(prices[i]-current_min>max_dif){
-                max_dif=prices[i]-current_min;                
-            }
+            current_min=min(current_min,prices[i]);
+            max_dif=max(max_dif,prices[i]-current_min);  
         }
         return max_dif;
     }
