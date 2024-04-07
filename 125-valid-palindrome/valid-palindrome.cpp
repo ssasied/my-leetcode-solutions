4bce7d723b45c10a8f214c03cc30b0fa7f150fc3
ssasied <115064635+ssasied@users.noreply.github.com>
2024-04-07 16:43:15 +0200

    Time: 4 ms (80.10%) | Memory: 8.7 MB (63.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -9,13 +9,13 @@ public:
             s[r]=tolower(s[r]);
             while(!((s[l]>='a'&&s[l]<='z')||(s[l]>='0'&&s[l]<='9'))&&l<r){                
                 l++;
-            s[l]=tolower(s[l]);
+                s[l]=tolower(s[l]);
             }
             while(!((s[r]>='a'&&s[r]<='z')||(s[r]>='0'&&s[r]<='9'))&&r>l){
                 r--;                
                 s[r]=tolower(s[r]);
             }
-            if(!(s[l]==s[r])){isp=false;cout<<s[l]<<l<<' '<<s[r]<<r;break;}
+            if(!(s[l]==s[r])){isp=false;break;}
             l++;
             r--;
         }
