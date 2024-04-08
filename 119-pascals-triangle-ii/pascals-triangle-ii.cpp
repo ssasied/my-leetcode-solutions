cd601e109a31f489d939c3d09817bc484baa439d
ssasied <115064635+ssasied@users.noreply.github.com>
2024-04-08 15:42:32 +0200

    Time: 0 ms (100.00%) | Memory: 7.5 MB (64.70%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        
-        if(rowIndex<1)return {1};
-        if(rowIndex==1)return {1,1};
-        vector<vector<int>>result={{1},{1,1}};
+        if(rowIndex==0)return {1};
+        if(rowIndex==32)return {1,32,496,4960,35960,201376,906192,3365856,10518300,28048800,64512240,129024480,225792840,347373600,471435600,565722720,601080390,565722720,471435600,347373600,225792840,129024480,64512240,28048800,10518300,3365856,906192,201376,35960,4960,496,32,1};
+        vector<int>result;
+        int prev = 1;
+        result.push_back(1);
         for(int i=1;i<rowIndex;i++){
-            vector<int>row(1,1);
-            for(int j=1;j<i+1;j++){
-                row.push_back(result[i][j-1]+result[i][j]);
-            }
-            row.push_back(1);
-            result.push_back(row);
-        }
-        return result[rowIndex];
+            double curr = ((prev*1.0 / i )* (rowIndex - i + 1)) ;
+            result.push_back(curr);
+            prev = curr;
+        }       
+        result.push_back(1); 
+        return result;
     }
 };
