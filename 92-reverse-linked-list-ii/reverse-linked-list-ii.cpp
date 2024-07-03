28fc54da6f90b34f6febca2e49311ea57e62c126
ssasied <115064635+ssasied@users.noreply.github.com>
2024-07-03 23:11:48 +0200

    Time: 0 ms (100.00%) | Memory: 9.5 MB (14.44%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -11,33 +11,23 @@
 class Solution {
 public:
    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (!head || left == right) return head;
+
         ListNode* dummy = new ListNode(0, head);
-        ListNode* current = dummy;
-
-        for(int i = 0; i < left - 1; i++){
-            current = current->next;
+        ListNode* prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev->next;
+        }
+        ListNode* start = prev->next;
+        ListNode* then = start->next;
+        for (int i = 0; i < right - left; i++) {
+            start->next = then->next;
+            then->next = prev->next;
+            prev->next = then;
+            then = start->next;
         }
 
-        ListNode* Left = current;
+        return dummy->next;
 
-        current = current->next;
-
-        std::vector<ListNode*> group;
-        for(int i = 0; i < right - left + 1; i++){
-            group.push_back(current);
-            current = current->next;
-        }
-
-        ListNode* Right = current;
-
-        int gs = group.size();
-        for(int i = gs - 1; i >= 0; i--){
-            Left->next = group[i];
-            Left = Left->next;
-        }
-
-        Left->next = Right;
-
-        return dummy->next; 
     }
 };
